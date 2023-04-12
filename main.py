68bfc5757e9111077f99e56c3a983d4a1b2f8fb7
pattarinn <pattarin.wn@gmail.com>
2023-04-12 16:35:06 +0700

    edit logic

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from camera import Camera
 from utils import BACKEND_URL, PATH_TO_SAVE_IMAGES, DATE_FORMAT, TIME_FORMAT
 
 
-
 while True:
     # camera.capture_image
     Camera.capture_image()
@@ -21,18 +20,20 @@ while True:
     time_string = now.strftime(TIME_FORMAT)
     # ex: camera_images/2023-03-05/00:07:40.png
     path = f'{PATH_TO_SAVE_IMAGES + "/" + now.strftime(DATE_FORMAT) + "/"+ time_string}.jpg'
-    
-    # save every image whether or not an elephant is found
-    Camera.save_image(path)
+    save_img = False
     
     if Camera.find_elephant(): # return true if an elephant is found on the pic that has been scanned
         # sent req to backend
         req_obj = {"informant": "camera-1"}
         response = requests.post(url=f"{BACKEND_URL}/record", json=req_obj)
         print(response)
-        
+        # save every image whether or not an elephant is found
+        Camera.save_image(path)
+        save_img = True
         google_drive_handler.upload_picture(path)
     
+    if not save_img:
+        Camera.save_image(path)
     
     # delete old folder
     dirs = os.listdir(PATH_TO_SAVE_IMAGES)
